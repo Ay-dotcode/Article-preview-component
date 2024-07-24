464e22df55dbeca0032af3cc2343f2ca9d71f309
Ay-dotcode <joseyowolabi@gmail.com>
2024-07-24 18:21:08 +0300

    finished setting up desktop view

--- script.js
+++ script.js
@@ -2,12 +2,16 @@ const footer1 = document.getElementById('footer1');
 const footer2 = document.getElementById('footer2');
 const shareBox1 = document.getElementById('shareBox1');
 const shareBox2 = document.getElementById('shareBox2');
+const arrow = document.getElementById('arrow');
 
 function shareOn() {
     footer2.style.display = 'flex';
+    arrow.style.display = 'flex'
+
 }
 function shareOff() {
     footer2.style.display = 'none';
+    arrow.style.display = 'none';
 }
 
 shareBox1.addEventListener("click", shareOn);
