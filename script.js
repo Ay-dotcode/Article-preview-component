cb8c1b13ecd510e85492e235d188999f8bfc48b7
Ay-dotcode <joseyowolabi@gmail.com>
2024-07-24 17:11:28 +0300

    Changed the 2nd footer to position: absolute

--- script.js
+++ script.js
@@ -4,12 +4,10 @@ const shareBox1 = document.getElementById('shareBox1');
 const shareBox2 = document.getElementById('shareBox2');
 
 function shareOn() {
-    footer1.style.display = 'none';
     footer2.style.display = 'flex';
 }
 function shareOff() {
     footer2.style.display = 'none';
-    footer1.style.display = 'flex';
 }
 
 shareBox1.addEventListener("click", shareOn);
